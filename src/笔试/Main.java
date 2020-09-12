2f4e8ee16fc2214efbbe7bc8cdcb466a1d808882
Alita Alice <3441425824@qq.com>
2020-09-12 14:31:56 +0800

    [mod]update

--- src/笔试/Main.java
+++ src/笔试/Main.java
@@ -6,24 +6,16 @@ import java.util.Scanner;
 public class Main {
     public static long binaryTodecimal2(String n) {
       String res=  Integer.valueOf(n,2).toString();
-      long in = Integer.parseInt(res);
+      //BigInteger in = new BigInteger(res);
+        long in = Long.parseLong(n);
         return in;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        long ss = 0;
-        try {
-             ss = Integer.parseUnsignedInt(str);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return;
-        }
-        if (ss >= 0 && ss < Math.pow(2, 100)) {
-            long source = binaryTodecimal2(str);
+//        if (ss >= 0 && ss < Math.pow(2, 100)) {
+        long source = binaryTodecimal2(str);
             //int source = scanner.nextInt();
             double target = Math.pow(source, 0.25);
             if ((long) target == target) {
@@ -31,8 +23,8 @@ public class Main {
             } else {
                 System.out.println((long) target + 1);
             }
-        } else {
-            return;
-        }
+//        } else {
+//            return;
+//        }
     }
 }
