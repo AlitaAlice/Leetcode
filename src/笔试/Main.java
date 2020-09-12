2312ea557033346afc9b9ccf7226190b88952f46
Alita Alice <3441425824@qq.com>
2020-09-12 14:07:18 +0800

    [mod]update

--- src/笔试/Main.java
+++ src/笔试/Main.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static int binaryTodecimal2(String n) {
+    public static long binaryTodecimal2(String n) {
       String res=  Integer.valueOf(n,2).toString();
-      int in = Integer.parseInt(res);
+      long in = Integer.parseInt(res);
         return in;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        int source = binaryTodecimal2(str);
+        long source = binaryTodecimal2(str);
         //int source = scanner.nextInt();
         double target = Math.pow(source, 0.25);
-        if ((int)target==target)
+        if ((long)target==target)
         {
-            System.out.println((int)target);
+            System.out.println((long)target);
         }
         else
         {
-            System.out.println((int)target+1);
+            System.out.println((long)target+1);
         }
       //  System.out.println(target);
     }
--- src/笔试/Solution.java
+++ src/笔试/Solution.java
@@ -15,21 +15,6 @@ public class Solution {
         System.out.println(sum(3,5));
     }
 
-//    int sum = 0, a, n, pre = 0;
-//    Scanner sc = new Scanner(System.in);
-//		System.out.println("请输入项数：");
-//    n = sc.nextInt();
-//		System.out.println("请输入数值：");
-//    a = sc.nextInt();
-//		for ( int i = 1; i <= n; i++ ) {
-//
-//        pre += a;
-//        sum += pre;
-//        a *= 10;
-//
-//    }
-//
-//		System.out.println("结果："+sum);
     public  static  long sum(int num, int itemNum) {
         long sum = 0, pre = 0;
         for (long i = 1; i <= itemNum; i++) {
