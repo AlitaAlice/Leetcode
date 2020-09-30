6668ae1045f3fb4459e2acd892b5f13d5212336d
Alita Alice <3441425824@qq.com>
2020-09-30 11:16:46 +0800

    [mod]update

--- src/动态规划/q118_杨辉三角/Solution.java
+++ src/动态规划/q118_杨辉三角/Solution.java
@@ -1,33 +1,40 @@
 import java.util.*;
-public class Main{
-    public static String reverse(String str , int start , int end){
-        char[] arr = str.toCharArray();
-        while (start < end ){
-            char tmp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = tmp;
-            start++;
-            end--;
-        }
-        return String.copyValueOf(arr);
-    }
-    public static String func(String str , int k){
-        if (str == null || k <= 0 || str.length() == 0){
-            return null;
-        }
-        str =  reverse(str , 0 , k-1);
-        str = reverse(str , k , str.length()-1);
-        str = reverse(str, 0 , str.length()-1);
-        return str;
-    }
-    public static void main (String[] args){
-        Scanner scan = new Scanner(System.in);
-        while(scan.hasNext()) {
-            int k = scan.nextInt();
-            String str = scan.next();
-            String ret = func(str,k);
-            System.out.println(ret);
-        }
 
+
+public class Solution {
+    /**
+     * return a array which include all ans for op3
+     * @param op int整型二维数组 operator
+     * @return int整型一维数组
+     */
+    //ArrayDeque<Integer> data=new ArrayDeque<>();
+    ArrayDeque<Integer> min=new ArrayDeque<>();
+    public int[] getMinStack (int[][] op) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int[] temp:op){
+            int first=temp[0];
+            if(first==1){
+                //data.push(temp[1]);
+                if(min.isEmpty()||temp[1]<min.peek()){
+                    min.push(temp[1]);
+                }
+                else{
+                    min.push(min.peek());
+                }
+            }
+            else if(first==2){
+                //data.pop();
+                min.pop();
+            }
+            else{
+                list.add(min.peek());
+            }
+        }
+        int[] res=new int[list.size()];
+        for(int i=0;i<res.length;i++){
+            res[i]=list.get(i);
+        }
+        return res;
     }
+
 }
